cf902610b0743c244e35ed15261e66c07d4fe9e7
Surya Gaddipati <sgaddipati@groupon.com>
2018-02-11 17:22:17 -0600

    main

--- src/main/java/suryagaddipati/cupcakecd/ContainerLogStreamer.java
+++ src/main/java/suryagaddipati/cupcakecd/ContainerLogStreamer.java
@@ -1,3 +1,48 @@
 package suryagaddipati.cupcakecd;
+
+import akka.actor.ActorSystem;
+import com.typesafe.config.ConfigFactory;
+import suryagaddipati.cupcakecd.docker.api.DockerApiRequest;
+import suryagaddipati.cupcakecd.docker.api.response.ApiException;
+import suryagaddipati.cupcakecd.docker.api.response.SerializationException;
+import suryagaddipati.cupcakecd.docker.api.service.ListServicesRequest;
+
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
 public class ContainerLogStreamer {
+        public static void main(String args[]) throws ExecutionException, InterruptedException {
+
+        final ActorSystem as = ActorSystem.create("swarm-plugin", ConfigFactory.load());;
+
+        final CompletionStage<Object> nodesStage = new DockerApiRequest(as, new ListServicesRequest("http://localhost:2376","label","ROLE=jenkins-agent")).execute();
+
+        Object o = nodesStage.toCompletableFuture().get();
+        if(o instanceof  ApiException){
+            ((ApiException)o).getCause().printStackTrace();
+        }
+
+        System.out.print(o);
+        as.terminate();
+    }
+
+    private static <T> T  getFuture(final CompletionStage<Object> future, Class<T> clazz) {
+        try {
+            final Object result = future.toCompletableFuture().get(5, TimeUnit.SECONDS);
+            return getResult(result,clazz);
+        } catch (InterruptedException|ExecutionException |TimeoutException e) {
+            throw  new RuntimeException(e);
+        }
+    }
+    private static <T> T  getResult(Object result, Class<T> clazz){
+        if(result instanceof SerializationException){
+            throw new RuntimeException (((SerializationException)result).getCause());
+        }
+        if(result instanceof ApiException){
+            throw new RuntimeException (((ApiException)result).getCause());
+        }
+        return clazz.cast(result);
+    }
 }
--- src/main/java/suryagaddipati/cupcakecd/docker/api/service/ListServicesRequest.java
+++ src/main/java/suryagaddipati/cupcakecd/docker/api/service/ListServicesRequest.java
@@ -23,55 +23,6 @@ public class ListServicesRequest extends ApiRequest {
        this(dockerApiUrl, "/services?filters="+encodeJsonFilter(filterKey,filterValue));
     }
 
-//    public static void main(String args[]) throws ExecutionException, InterruptedException {
-//
-//        final ActorSystem as = ActorSystem.create("swarm-plugin", ConfigFactory.load());;
-//
-//        final CompletionStage<Object> nodesStage = new suryagaddipati.jenkinsdockerslaves.docker.api.DockerApiRequest(as, new ListServicesRequest("http://localhost:2376","label","ROLE=jenkins-agent")).execute();
-//        CompletionStage<Object> tasksStage = nodesStage.thenApplyAsync(services -> {
-//            if(services instanceof ApiException){
-//                return services;
-//            }
-//            ((List<ScheduledService>)services).stream().filter(service -> {
-//
-//                return false;
-//            });
-//            for(ScheduledService service : (List<ScheduledService>)services ){
-//                CompletionStage<Object> tasksRequest = new suryagaddipati.jenkinsdockerslaves.docker.api.DockerApiRequest(as, new ListTasksRequest("http://localhost:2376", "service", service.Spec.Name)).execute();
-//                List<Task> tasks = getFuture(tasksRequest, List.class);
-//                if(tasks != null && tasks.size()==1) {
-//                    Task task = tasks.get(0);
-//                    System.out.print(task.isComplete());
-//                }
-//                System.out.println(service.Spec.Name);
-//            }
-//            return services;
-//        });
-//        Object o = tasksStage.toCompletableFuture().get();
-//        if(o instanceof  ApiException){
-//            ((ApiException)o).getCause().printStackTrace();
-//        }
-//
-//        System.out.print(o);
-//        as.terminate();
-//    }
 
-    private static <T> T  getFuture(final CompletionStage<Object> future,Class<T> clazz) {
-        try {
-            final Object result = future.toCompletableFuture().get(5, TimeUnit.SECONDS);
-            return getResult(result,clazz);
-        } catch (InterruptedException|ExecutionException |TimeoutException e) {
-            throw  new RuntimeException(e);
-        }
-    }
-    private static <T> T  getResult(Object result, Class<T> clazz){
-        if(result instanceof SerializationException){
-            throw new RuntimeException (((SerializationException)result).getCause());
-        }
-        if(result instanceof ApiException){
-            throw new RuntimeException (((ApiException)result).getCause());
-        }
-        return clazz.cast(result);
-    }
 
 }
